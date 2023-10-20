a0b6ad26de7cd13d277375a099e479c51230134f
TheDevYellowy <yellowycorn6963@gmail.com>
2023-10-19 20:56:32 -0500

    hopefully the first and last commit

---
+++ index.ts
@@ -0,0 +1,23 @@
+import { existsSync, readFileSync } from "fs";
+
+Bun.serve({
+  port: 80,
+  async fetch(req): Promise<Response> {
+    const url = new URL(req.url);
+    console.log(`${url.pathname} : ${this.requestIP(req)?.address} : ${existsSync(`.${url.pathname}`)}`);
+    
+    if(url.pathname.includes('.png')) {
+      if(existsSync(`.${url.pathname}`)) return new Response(readFileSync(`.${url.pathname}`), { status: 200, headers: {
+        'content-type': 'image/png'
+      } });
+    } else if(url.pathname.includes('.jpg')) {
+      if(existsSync(`.${url.pathname}`)) return new Response(readFileSync(`.${url.pathname}`), { status: 200, headers: {
+        'content-type': 'image/jpeg'
+      }});
+    }
+
+    return new Response(null, { status: 204 });
+  }
+});
+
+console.log(`CDN up and running`);
